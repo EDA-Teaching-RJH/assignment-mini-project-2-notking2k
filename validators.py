47cab8e75656cda9d6e125aa7e3a8775a6e39fd7
notking2k <kkm@kent.ac.uk>
2026-03-27 22:29:19 +0000

    i then changed it to be able ot handle extra spaces

--- validators.py
+++ validators.py
@@ -2,3 +2,7 @@ import re
 def clean_name(name):
     name = re.sub(r"[^A-Za-z\s]", "", name)
     return name.strip().title()
+def clean_name(name):
+    name = re.sub(r"[^A-Za-z\s]", "", name)
+    name = re.sub(r"\s+", " ", name)
+    return name.strip().title()
