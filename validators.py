d0cd21cf6b18df7d22f7dce4b637feac7b59321f
notking2k <kkm@kent.ac.uk>
2026-03-27 22:36:17 +0000

    then lastly added the function thta allow it to extract words and number from the text thats given

--- validators.py
+++ validators.py
@@ -10,3 +10,5 @@ def check_player_id(player_id):
     return re.match(r"^PLR\d{4}$", player_id) is not None
  def check_email(email):
     return re.fullmatch(r"[\w\.-]+@[\w\.-]+\.\w+", email) is not None
+def find_parts(text):
+    return re.findall(r"[A-Za-z0-9]+", text)
