6048488dd1b89ff8b3e8826ae5b9cf42194bceea
notking2k <kkm@kent.ac.uk>
2026-03-27 23:29:24 +0000

    ive decided to save players to csv

--- file_handler.py
+++ file_handler.py
@@ -1,1 +1,7 @@
 import csv
+def save_players(team):
+    with open("players.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["name", "id", "email"])
+    for p in team.players:
+        writer.writerow([p.name, p.player_id, p.email])       
