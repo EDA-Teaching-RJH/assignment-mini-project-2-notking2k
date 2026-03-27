e3891fab00cf2e2430909750bff89b50eb2c893d
notking2k <kkm@kent.ac.uk>
2026-03-27 22:23:47 +0000

    i started the validation file

--- validators.py
+++ validators.py
@@ -1,18 +1,1 @@
 import re
-
-
-def normalise_name(value):
-    value = re.sub(r"[^A-Za-z\s]", "", value)
-    value = re.sub(r"\s+", " ", value)
-    return value.strip().title()
-
-def player_code_ok(value):
-    pattern = r"^PLR\d{4}$"
-    return re.match(pattern, value) is not None
-
-def email_ok(value):
-    pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
-    return re.fullmatch(pattern, value) is not None
-
-def get_bits(text):
-    return re.findall(r"[A-Za-z0-9]+", text) 
