869e793dfefd82e6a8606203de9616833f87d507
notking2k <kkm@kent.ac.uk>
2026-03-27 21:38:23 +0000

    overall i made player and striker classes will pretty simple stat tracking aswell as team class for u to keep and manage players

--- models.py
+++ models.py
@@ -30,3 +30,50 @@ class SquadMember:
             "role": "SquadMember",
             "extra": ""
         }
+   def __str__(self):
+        return f"{self.name} - {self.code} - Goals: {self.goals} - Assists: {self.assists}"
+
+
+class GoalPoacher(SquadMember):
+    def __init__(self, name, code, email, shots=0):
+        super().__init__(name, code, email)
+        self.shots = shots
+
+    def record_shot(self):
+        self.shots = self.shots + 1
+
+    def pack(self):
+        data = super().pack()
+        data["role"] = "GoalPoacher"
+        data["extra"] = self.shots
+        return data
+
+    def __str__(self):
+        return super().__str__() + f" - Shots: {self.shots}"
+
+
+class ClubRegister:
+    def __init__(self):
+        self.entries = []
+
+    def insert(self, member):
+        for current in self.entries:
+            if current.code == member.code:
+                raise ValueError("That player code already exists")
+        self.entries.append(member)
+
+    def fetch(self, code):
+        for current in self.entries:
+            if current.code == code:
+                return current
+        return None
+
+    def erase(self, code):
+        for i in range(len(self.entries)):
+            if self.entries[i].code == code:
+                self.entries.pop(i)
+                return True
+        return False
+
+    def listing(self):
+        return self.entries  
