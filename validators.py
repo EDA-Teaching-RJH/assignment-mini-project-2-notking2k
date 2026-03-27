e73f9b84cb66939eb8a6e74fc04df2e8bd8e7ead
notking2k <kkm@kent.ac.uk>
2026-03-27 22:34:09 +0000

    i went onto adding the player ide validation using regex

--- validators.py
+++ validators.py
@@ -6,3 +6,5 @@ def clean_name(name):
     name = re.sub(r"[^A-Za-z\s]", "", name)
     name = re.sub(r"\s+", " ", name)
     return name.strip().title()
+def check_player_id(player_id):
+    return re.match(r"^PLR\d{4}$", player_id) is not None
